c88b218e4d8e34aa02afe53449c27ae473123388
Syed Ibrahim <syedibrahim.developer@gmail.com>
2025-09-08 10:07:45 +0530

    pydantic model_config

--- pydantic_models/BookRequest.py
+++ pydantic_models/BookRequest.py
@@ -4,9 +4,21 @@ from pydantic import BaseModel, Field
 
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description="id is not needed", default=None)
     name: str = Field(min_length=3)
     author: str = Field(min_length=3)
     rating: int = Field(gt=0, lt=6)
     price: int = Field(gt=0, lt=1000)
     description: str = Field(min_length=10)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "name": "A new book",
+                "author": "A new Author",
+                "rating": 3,
+                "price": 500,
+                "description": "A sample description"
+            }
+        }
+    }
